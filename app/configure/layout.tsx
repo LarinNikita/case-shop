7201daa8c39718a70da3b04b1fd9e75c8b6156c1
LarinNikita <larin_nikita_m@mail.ru>
2024-06-19 13:17:30 +0700

    steps ui

--- app/configure/layout.tsx
+++ app/configure/layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import Steps from '@/components/Steps'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <MaxWidthWrapper className="flex flex-1 flex-col">
+            <Steps />
             {children}
         </MaxWidthWrapper>
     )
