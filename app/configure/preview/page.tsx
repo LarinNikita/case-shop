9745fd61185dcb0ac29a1bd5f6b87f5f66d7d816
LarinNikita <larin_nikita_m@mail.ru>
2024-06-20 14:52:54 +0700

    summary page ui

---
+++ app/configure/preview/page.tsx
@@ -0,0 +1,29 @@
+import { notFound } from 'next/navigation'
+
+import { db } from '@/db'
+
+import DesignPreview from './_components/DesignPreview'
+
+interface PageProps {
+    searchParams: {
+        [key: string]: string | string[] | undefined
+    }
+}
+
+export default async function Page({ searchParams }: PageProps) {
+    const { id } = searchParams
+
+    if (!id || typeof id !== 'string') {
+        return notFound()
+    }
+
+    const configuration = await db.configuration.findUnique({
+        where: { id },
+    })
+
+    if (!configuration) {
+        return notFound()
+    }
+
+    return <DesignPreview configuration={configuration} />
+}
