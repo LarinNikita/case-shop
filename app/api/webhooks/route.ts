7ad2edea12ff1e7000543fcd02b01c2bd4aaf049
LarinNikita <larin_nikita_m@mail.ru>
2024-06-21 15:59:09 +0700

    setup react email and resend. custom email templete

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,10 +1,14 @@
 import Stripe from 'stripe'
+import { Resend } from 'resend'
 import { headers } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
 import { db } from '@/db'
 
 import { stripe } from '@/lib/stripe'
+import OrderReceivedEmail from '@/components/emails/OrderReceivedEmail'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: NextRequest) {
     try {
@@ -40,7 +44,7 @@ export async function POST(req: NextRequest) {
             const billingAddress = session.customer_details!.address
             const shippingAddress = session.shipping_details!.address
 
-            await db.order.update({
+            const updatedOrder = await db.order.update({
                 where: { id: orderId },
                 data: {
                     isPaid: true,
@@ -65,6 +69,26 @@ export async function POST(req: NextRequest) {
                         },
                     },
                 },
+            })
+
+            await resend.emails.send({
+                from: 'CaseCobra <assistant5tzeb5k@gmail.com>',
+                to: [event.data.object.customer_details!.email!],
+                subject: 'Thanks for you order!',
+                react: OrderReceivedEmail({
+                    orderId,
+                    userId,
+                    orderDate: updatedOrder.createdAt.toLocaleDateString(),
+                    //@ts-ignore
+                    shippingAddress: {
+                        name: session.customer_details!.name!,
+                        city: shippingAddress!.city!,
+                        country: shippingAddress!.country!,
+                        postalCode: shippingAddress!.postal_code!,
+                        street: shippingAddress!.line1!,
+                        state: shippingAddress!.state!,
+                    },
+                }),
             })
         }
 
